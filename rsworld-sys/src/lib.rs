8b3d6a628ab4094637a6611cf240d23a7463ef89
White-Green <43771790+White-Green@users.noreply.github.com>
2023-01-10 03:52:22 +0900

    add initialize helper for rsworld-sys

--- rsworld-sys/src/lib.rs
+++ rsworld-sys/src/lib.rs
@@ -1,13 +1,14 @@
 include!(concat!(env!("OUT_DIR"), "/bindgen.rs"));
 
-use std::os::raw::{c_int, c_double};
+use std::mem::MaybeUninit;
+use std::os::raw::c_int;
 
 impl CheapTrickOption {
     pub fn new(fs: c_int) -> Self {
         unsafe {
-            let mut option = std::mem::MaybeUninit::uninit().assume_init();
-            InitializeCheapTrickOption(fs, &mut option as *mut _);
-            option
+            let mut value = MaybeUninit::uninit();
+            InitializeCheapTrickOption(fs, value.as_mut_ptr());
+            value.assume_init()
         }
     }
 }
@@ -15,9 +16,9 @@ impl CheapTrickOption {
 impl D4COption {
     pub fn new() -> Self {
         unsafe {
-            let mut option:D4COption = std::mem::MaybeUninit::uninit().assume_init();
-            InitializeD4COption(&mut option as *mut _);
-            option
+            let mut value = MaybeUninit::uninit();
+            InitializeD4COption(value.as_mut_ptr());
+            value.assume_init()
         }
     }
 }
@@ -25,9 +26,19 @@ impl D4COption {
 impl DioOption {
     pub fn new() -> Self {
         unsafe {
-            let mut option:DioOption = std::mem::MaybeUninit::uninit().assume_init();
-            InitializeDioOption(&mut option as *mut _);
-            option
+            let mut value = MaybeUninit::uninit();
+            InitializeDioOption(value.as_mut_ptr());
+            value.assume_init()
+        }
+    }
+}
+
+impl ForwardRealFFT {
+    pub fn new(fft_size: c_int) -> Self {
+        unsafe {
+            let mut value = MaybeUninit::uninit();
+            InitializeForwardRealFFT(fft_size, value.as_mut_ptr());
+            value.assume_init()
         }
     }
 }
@@ -35,9 +46,62 @@ impl DioOption {
 impl HarvestOption {
     pub fn new() -> Self {
         unsafe {
-            let mut option:HarvestOption = std::mem::MaybeUninit::uninit().assume_init();
-            InitializeHarvestOption(&mut option as *mut _);
-            option
+            let mut value = MaybeUninit::uninit();
+            InitializeHarvestOption(value.as_mut_ptr());
+            value.assume_init()
+        }
+    }
+}
+
+impl InverseComplexFFT {
+    pub fn new(fft_size: c_int) -> Self {
+        unsafe {
+            let mut value = MaybeUninit::uninit();
+            InitializeInverseComplexFFT(fft_size, value.as_mut_ptr());
+            value.assume_init()
+        }
+    }
+}
+
+impl InverseRealFFT {
+    pub fn new(fft_size: c_int) -> Self {
+        unsafe {
+            let mut value = MaybeUninit::uninit();
+            InitializeInverseRealFFT(fft_size, value.as_mut_ptr());
+            value.assume_init()
+        }
+    }
+}
+
+impl MinimumPhaseAnalysis {
+    pub fn new(fft_size: c_int) -> Self {
+        unsafe {
+            let mut value = MaybeUninit::uninit();
+            InitializeMinimumPhaseAnalysis(fft_size, value.as_mut_ptr());
+            value.assume_init()
+        }
+    }
+}
+
+impl WorldSynthesizer {
+    pub fn new(
+        fs: c_int,
+        frame_period: f64,
+        fft_size: c_int,
+        buffer_size: c_int,
+        number_of_pointers: c_int,
+    ) -> Self {
+        unsafe {
+            let mut value = MaybeUninit::uninit();
+            InitializeSynthesizer(
+                fs,
+                frame_period,
+                fft_size,
+                buffer_size,
+                number_of_pointers,
+                value.as_mut_ptr(),
+            );
+            value.assume_init()
         }
     }
 }
